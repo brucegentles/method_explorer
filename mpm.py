549b06f435a22ad8c9883e43397cbb1e111db5b0
Bruce Gentles <bruce@kbgentles.org.uk>
2016-05-16 22:45:49 +0100

    moved method functions into method module
    added bob minor as worked example which should be true...

--- mpm.py
+++ mpm.py
@@ -7,8 +7,37 @@ notated as X = swap and | = lie.
 Plain hunt would be (XXX,|XX|) repeated continously.
 """
 
-import numpy as np;
+import method
 
+bob_minor_lead = [
+    123456,
+    214365,
+    241635,
+    426153,
+    462513,
+    645231,
+    654321,
+    563412,
+    536142,
+    351624,
+    315264,
+    132546,
+    123456,
+]
+bob_minor_plain = [
+    132546,
+    135264,
+]
+bob_minor_bob = [
+    132546,
+    123564,
+]
+bob_minor_single = [
+    132546,
+    132564,
+]
+
+# 13 orders, 12 changes. 5 x 12 = 60
 lead6_order = [
     123456,
     213465,
@@ -107,273 +136,31 @@ single_order = [
     # 516234,
 ]
 
-VERBOSE = False
-N = 6
-PLACE_RANGE = range(0, N)
-ROUNDS = range(1, N + 1)
-
-
-def is_rounds(order):
-    for i in PLACE_RANGE:
-        if order[i] != ROUNDS[i]:
-            return False
-    print "found rounds"
-    return True
-
-
-def zeros(n):
-    a = list()
-    for i in range(0, n):
-        a.append(0)
-    return a
-
-
-def print_notes(n):
-    for e in n:
-        str = ""
-        for s in e:
-            str += s
-        print str
-
-
-def places_to_order(places):
-    n = 0
-    for v in places:
-        n *= 10
-        n += v
-    return n
-
-def print_orders(n):
-    i = 0
-    for e in n:
-        n = places_to_order(e)
-        i += 1
-        print i, " : ", n
-
-
-def order_to_places(order):
-    """
-    converts a series of decimal numbers,
-    as the place order in decimal notation, to a 2d array of individual bell places
-    :param order: list of integer bell orders
-    :return: array or places to write to
-    """
-    places = []
-    for ch in order:
-        # print "order =",ch
-        pl = zeros(N)
-        i = 5
-        while ch > 0:
-            p2 = int(ch / 10)
-            p3 = p2 * 10
-            p = ch - p3
-            ch = p2
-            # print "place =",p
-            pl[i] = p
-            i -= 1
-        places.append(pl)
-    return places
-
-
-def lead_to_changes(lead):
-    """
-    takes a list of orders, as a lead end, and translates into a list of changes.
-    :param lead:
-    :return: changes
-    """
-    changes = []
-    pls = order_to_places(lead)
-    n = len(pls)
-    for i in range(1, n):
-        now = pls[i]
-        prev = pls[i - 1]
-        # print "change\n\t",prev, "->\n\t", now
-        note = calc_change(prev, now)
-        changes.append(note)
-    return changes
-
-
-def calc_change(prev, now):
-    """
-    calculate the change notation for a pair of places
-    :param prev:  places before
-    :param now:   places after change
-    :return: note string.
-    """
-    m = len(prev)
-    note = []
-    pl = 0
-    while pl < m:
-        if prev[pl] == now[pl]:
-            note.append("|")
-        elif prev[pl] == now[pl + 1] and prev[pl + 1] == now[pl]:
-            note.append("X")
-            pl += 1
-        else:
-            print "illegal change", prev[pl], now[pl + 1], prev[pl + 1], now[pl]
-        pl += 1
-    return note
-
-
-def check_leads(leads):
-    """
-    Checks the members of leads to see that they all have the same set of changes.
-    :return:
-    """
-    ch_set = []
-    for lead in leads:
-        ch = lead_to_changes(lead)
-        ch_set.append(ch)
-    nx = len(ch_set)
-    ny = len(ch_set[0])
-    ok = 0
-    bad = 0
-    done = 0
-    for x in range(1, nx):
-        for y in range(0, ny):
-            done += 1
-            if ch_set[x][y] != ch_set[x - 1][y]:
-                bad += 1
-            else:
-                ok += 1
-    if bad > 0:
-        print ok, "/", done, " good, ", bad, "/", done, " bad"
-    return bad == 0
-
-
-def swap(order, index):
-    """
-    swap elements [index] and [index+1] of order
-    :param order: input order
-    :param index:
-    :return: none
-    """
-    tmp = order[index]
-    order[index] = order[index + 1]
-    order[index + 1] = tmp
-
-
-def do_change(order, change):
-    """
-    interpret one change string and apply to place in to make place out
-    :param order: places before - modified in-plce
-    :param change: change string.
-    :return:
-    """
-    i = 0
-    if VERBOSE:
-        print "before", order, "change", change
-    for c in change:
-        if i >= len(order):
-            print "got to end of change"
-            break
-        # print "a:",c,i
-        if c == 'X':
-            swap(order, i)
-            i += 2
-        elif c == '|':
-            i += 1
-        else:
-            print "ERROR bad change symbol:", c
-            # print "b:",c,i
-    if VERBOSE:
-        print "after", order, "change", change
-
-
-def append_order(order, l):
-    new = []
-    for x in order:
-        new.append(x)
-    l.append(new)
-
-
-def copy_order(order):
-    new = []
-    for x in order:
-        new.append(x)
-    return new
-
-
-def check_rounds(order, till_rounds):
-    if till_rounds:
-        if is_rounds(order):
-            return True
-    return False
-
-
-def play_lead(order, lead, cmd, till_rounds, result):
-    """
-    play a single lead, mofifying the last element with the change for the given cmd.
-    if Till_rounds is set, abort if rounds is got to.
-    :param order: current order of bells. list of N places
-    :param lead: list of changes for plain course
-    :param cmd: change for this type of lead - ie the bob change.
-    :param till_rounds: boolean, if True, will repeat until order is rounds.
-    :param result: resulting list of orders to be appended to
-    :return: True if we have not yet come into rounds.
-    """
-    my_lead = lead
-    my_lead[-1] = cmd[0]
-    if VERBOSE:
-        print lead[-1]
-        print my_lead[-1]
-    for change in my_lead:
-        do_change(order, change)
-        append_order(order, result)
-        if check_rounds(order, till_rounds):
-            return False
-    if check_rounds(order, till_rounds):
-        return False
-    return True
-
-
-def play_composition(comp, method, till_rounds):
-    """
-    sequences a composition
-    :param comp:  composition, a list of verbs from the set plain,bob,single
-    :param till_rounds: boolean, if True, will repeat until order is rounds.
-    :return: list of orders.
-    """
-    res = []
-    order = copy_order(ROUNDS)
-    more = True
-    while more:
-        for cmd in comp:
-            more = play_lead(order, method["lead"], method[cmd], till_rounds, res)
-            if not more:
-                break
-        if not till_rounds:
-            break
-    return res
-
-
-def check_method(orders):
-    """
-    checks to see if all changes have been sone, or there are any duplicates
-    :param orders: orders, list of places.
-    :return: Boolean, True if no repeats
-    """
-    hist = np.zeros(654321)
-    n_rep = 0
-    for i in orders:
-        n = places_to_order(i)
-        hist[n] += 1
-        if (hist[n] > 1.0):
-            n_rep += 1
-            print "repeat", n, hist[n]
-    return n_rep
-
-if False:
-    ok = check_leads(leads)
-    print "ok=", ok
-
-method = {
-    "lead": lead_to_changes(lead2_order),
-    "bob": lead_to_changes(bob_order),
-    "single": lead_to_changes(single_order),
-    "plain": lead_to_changes(plain_order)
+method_bm = {
+    "lead": method.lead_to_changes(bob_minor_lead),
+    "bob": method.lead_to_changes(bob_minor_bob),
+    "single": method.lead_to_changes(bob_minor_single),
+    "plain": method.lead_to_changes(bob_minor_plain)
 }
 
+for s in method_bm["lead"]:
+    print s
+
+method_mpm = {
+    "lead": method.lead_to_changes(lead2_order),
+    "bob": method.lead_to_changes(bob_order),
+    "single": method.lead_to_changes(single_order),
+    "plain": method.lead_to_changes(plain_order)
+}
+
+for s in method_mpm["lead"]:
+    print s
+exit()
+
+
+if False:
+    ok = method.check_leads(leads)
+    print "ok=", ok
 
 # print_notes(method["lead"])
 
@@ -408,6 +195,39 @@ touch2 = [
     "plain", "plain", "plain",
 ]
 
-orders = play_composition(touch2, method, True)
-print_orders(orders)
-check_method(orders)
+touch720 = [
+    "plain", "plain", "bob",
+    "plain", "plain", "bob",
+    # "plain", "plain", "plain", "plain", "bob",
+    # "plain", "plain", "plain", "plain", "bob"
+]
+
+one_lead = ["bob", "plain", "plain", "plain",  # 48
+            "bob", "plain", "plain", "plain",  # 96
+            "bob", "plain", "plain", "plain",  # 144
+            "bob", "plain", "plain", "plain",  # 192
+            "bob", "plain", "plain", "single",  # 240
+            "bob", "plain", "plain", "plain",  # 48
+            "bob", "plain", "plain", "plain",  # 96
+            "bob", "plain", "plain", "plain",  # 144
+            "bob", "plain", "plain", "plain",  # 192
+            "bob", "plain", "plain", "plain",  # 240
+            "bob", "plain", "plain", "plain",  # 288
+            "bob", "plain", "plain", "plain",  # 336
+            "bob", "plain", "plain", "plain",  # 384
+            "bob", "plain", "plain", "plain",  # 432
+            "bob", "plain", "plain", "single",  # 480
+            "bob", "plain", "plain", "plain",  # 48
+            "bob", "plain", "plain", "plain",  # 96
+            "bob", "plain", "plain", "plain",  # 144
+            "bob", "plain", "plain", "plain",  # 192
+            "bob", "plain", "plain", "plain",  # 240
+            ]
+
+glenn_a_a_taylor_720 = [
+    "plain", "plain", "plain",
+    "bob", "single", "single"
+]
+orders = method.play_composition(one_lead, method_bm, True)
+method.print_orders(orders)
+method.check_method(orders)
